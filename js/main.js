59f3ee1c225bd07607addce1db04acfa49a58213
mikqmas <samkim86@gmail.com>
2016-08-16 11:25:37 -0700

    resize fixed

--- js/main.js
+++ js/main.js
@@ -1,31 +1,19 @@
 var contentDiv = document.getElementById("content"),
 pageDiv = document.getElementById("page"),
+containerDiv = document.getElementById("container"),
 samContainerDiv = document.getElementById("sam-container"),
 samDiv = document.getElementById("sam"),
-samFramesDiv = document.getElementById("sam-slides"),
-samEyesCloseDiv = document.getElementById("sam-eyes-close"),
+samSprite = document.getElementById("sam-sprite"),
 layerHorizontalArray = new Array,
 layerHorizontalSpeedArray = new Array,
-containerDiv = document.getElementById("container"),
 pageVerticalPosition = 0,
-samFramesDiv = document.getElementById("sam-slide"),
 deltaPageVerticalPosition = 0,
 previousPageVerticalPosition = 0,
-samEyesCloseDiv = document.getElementById("eyes-closed"),
-shiftSamFrameTimer,
-counter = 0,
-canAnimate,
-testFrames = document.getElementById("test"),
+isRunning = true,
 running,
 standing,
 timeout,
-test = document.getElementById("test"),
 count = 0;
-// function handleMove(e) {
-//   e.preventDefault();
-//   1 == canScrollOrSwipe && (detectPageVerticalPosition(),
-//   runTheseFunctionsAfterScrollOrSwipe())
-// }
 
 function setLayerSpeed() {
     for (; layerHorizontalSpeedArray.length > 0; ){
@@ -37,7 +25,6 @@ function setLayerSpeed() {
     }
 }
 
-
 function setPageHeight() {
     pageDiv.style.height = document.getElementById('layer-horizontal-3').offsetWidth - 1000 + "px"
 }
@@ -46,60 +33,58 @@ window.onload = function() {
   setPageHeight();
   storeDivs();
   setLayerSpeed();
-}
-
-window.onscroll = function(e) {
-    // detectPageVerticalPosition();
-    runTheseFunctionsAfterScrollOrSwipe();
+  runTheseFunctionsAfterScrollOrSwipe();
+  checkStatus();
 };
 
-function standingAni() {
-  timeout = setTimeout(function(){
-    if(count < 8 || count > 11) { count = 8;}
-    test.style.left = (count * -175) + "px";
-    count += 1;
-    standing = requestAnimationFrame(standingAni);
-  },(1000/5));
+window.onscroll = function(e) {
+  runTheseFunctionsAfterScrollOrSwipe();
+};
+
+window.onkeyup = function(e) {
+  if(e.key === "KeyUp" || e.key === "KeyDown") {
+    checkStatus();
+  }
+};
+
+window.onresize = function(e) {
+  setPageHeight();
+  storeDivs();
+  setLayerSpeed();
+  runTheseFunctionsAfterScrollOrSwipe();
+};
+
+function checkStatus(){
+  var checking = setInterval(function(){
+    detectPageVerticalPosition();
+    if(Math.abs(deltaPageVerticalPosition) < 5) {
+      isRunning = false;
+      cancelAnimationFrame(running);
+      clearTimeout(timeout);
+      standingAni();
+      clearInterval(checking);
+    }
+  },500);
 }
 
-$(document).on("scrollstop",function(){
-  cancelAnimationFrame(running);
-  clearTimeout(timeout);
-  standingAni();
-});
 
-function runningAni() {
-  timeout = setTimeout(function(){
-    if(count > 7) { count = 0; }
-    test.style.left = (count * -175) + "px";
-    count += 1;
-    running = requestAnimationFrame(runningAni);
-  },(1000/9));
+function runTheseFunctionsAfterScrollOrSwipe() {
+  detectPageVerticalPosition();
+  orientSam();
+  moveLayers();
+  if(isRunning === false && Math.abs(deltaPageVerticalPosition) > 5){
+    isRunning = true;
+    cancelAnimationFrame(standing);
+    clearTimeout(timeout);
+    runningAni();
+    checkStatus();
+  }
 }
-
-$(document).on("scrollstart",function(){
-  cancelAnimationFrame(standing);
-  clearTimeout(timeout);
-  runningAni();
-});
 
 function detectPageVerticalPosition(){
   previousPageVerticalPosition = pageVerticalPosition;
   pageVerticalPosition = window.scrollY;
   deltaPageVerticalPosition = pageVerticalPosition - previousPageVerticalPosition;
-}
-
-function runningAnimation() {
-  console.log(deltaPageVerticalPosition);
-
-}
-
-function runTheseFunctionsAfterScrollOrSwipe() {
-  detectPageVerticalPosition();
-  moveLayers();
-  orientSam();
-  runningAnimation();
-  // animateSam();
 }
 
 function moveLayers() {
@@ -115,40 +100,55 @@ function storeDivs() {
 }
 
 function orientSam() {
-    deltaPageVerticalPosition > 0 && (testFrames.style.top = "0px"),
-    0 > deltaPageVerticalPosition && (testFrames.style.top = "-200px")
+    deltaPageVerticalPosition > 0 && (samSprite.style.top = "0px"),
+    0 > deltaPageVerticalPosition && (samSprite.style.top = "-200px")
 }
 
-function animateSam() {
-  clearInterval(shiftSamFrameTimer),
-  shiftSamFrameTimer = setInterval(function() {
-    if(counter === 0){
-      counter += 1;
-    }else {
-      counter -= 1;
-    }
-    shiftSamFrame();
-  }, 200);
+
+function standingAni() {
+  timeout = setTimeout(function(){
+    if(count < 8 || count > 11) { count = 8;}
+    samSprite.style.left = (count * -175) + "px";
+    count += 1;
+    standing = requestAnimationFrame(standingAni);
+  },(1000/5));
 }
 
-function clearShiftFrameTimer() {
-    clearInterval(shiftSamFrameTimer),
-    setStaticFrame(),
-    counter = 0,
-    enableAnimate();
+// $(document).on("scrollstop",function(){
+//   console.log("stop");
+//   cancelAnimationFrame(running);
+//   clearTimeout(timeout);
+//   standingAni();
+// });
+
+function runningAni() {
+  timeout = setTimeout(function(){
+    if(count > 7) { count = 0; }
+    samSprite.style.left = (count * -175) + "px";
+    count += 1;
+    running = requestAnimationFrame(runningAni);
+  },(1000/9));
 }
 
-function enableAnimate() {
-    canAnimate = true;
-}
-function disableAnimate() {
-    canAnimate = false;
-}
-
-function shiftSamFrame() {
-  samFramesDiv.style.left = -1 * 200 * (1 + counter) + "px";
-}
-
-function setStaticFrame() {
-    samFramesDiv.style.left = "0px";
-}
+// $(document).on("scrollstart",function(){
+//   console.log("start");
+//   cancelAnimationFrame(standing);
+//   clearTimeout(timeout);
+//   runningAni();
+// });
+//
+// function animateSam() {
+//   clearInterval(shiftSamFrameTimer),
+//   shiftSamFrameTimer = setInterval(function() {
+//     if(counter === 0){
+//       counter += 1;
+//     }else {
+//       counter -= 1;
+//     }
+//     shiftSamFrame();
+//   }, 200);
+// }
+//
+// function shiftSamFrame() {
+//   samFramesDiv.style.left = -1 * 200 * (1 + counter) + "px";
+// }
